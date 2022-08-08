a20b8919446e1902f8df99024c8407a3e4d3761c
HyoinJeong <hiamy0107@naver.com>
2022-08-08 19:27:51 +0900

    SpringBoot JDBC 설정

---
+++ kdt-chapter1/src/test/java/com/kdt/lecture/JDBCTest.java
@@ -0,0 +1,56 @@
+package com.kdt.lecture;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+@Slf4j
+public class JDBCTest {
+
+    static final String JDBC_DRIVER = "org.h2.Driver";
+    static final String DB_URL = "jdbc:h2:~/test";
+    static final String USER = "sa";
+    static final String PASS = "";
+
+    static final String DROP_TABLE_SQL = "DROP TABLE customers IF EXISTS";
+    static final String CREATE_TABLE_SQL = "CREATE TABLE customers(id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))";
+    static final String INSERT_SQL = "INSERT INTO customers (id, first_name, last_name) VALUES(1, 'honggu', 'kang')";
+
+    @Test
+    void jdbc_sample() {
+        try {
+            Class.forName(JDBC_DRIVER);
+
+            Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
+            log.info("Connection 획득");
+            // CONNECTION
+
+            Statement statement = connection.createStatement();
+            // STATEMENT
+            log.info("Statement 획득");
+            log.info("쿼리 실행");
+            statement.executeUpdate(DROP_TABLE_SQL);
+            statement.executeUpdate(CREATE_TABLE_SQL);
+            log.info("CREATED TABLE");
+
+            statement.executeUpdate(INSERT_SQL);
+            log.info("INSERTED CUSTOMER INFORMATION");
+            ResultSet resultSet = statement.executeQuery("SELECT id, first_name, last_name FROM customers WHERE id = 1");
+            // RESULT_SET
+
+            while(resultSet.next()) {
+                log.info(resultSet.getString("first_name"));
+            }
+
+            log.info("반납, 반납");
+            statement.close();
+            connection.close();
+            // CLOSE
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
